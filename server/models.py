86f510283337625bbc9fd551bdd999ffd216c915
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-03-05 21:41:09 +0300

    final commit

--- server/models.py
+++ server/models.py
@@ -13,20 +13,19 @@ class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
 
-    pets=db.relationship('Pet', backref='owner')
+    pets = db.relationship('Pet', backref='owner')
 
     def __repr__(self):
         return f'<Pet Owner {self.name}>'
-    
+
 class Pet(db.Model):
     __tablename__ = 'pets'
-
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
     species = db.Column(db.String)
 
     owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
 
     def __repr__(self):
-        return f'<Pet {self.name}, {self.species}'
-    
+        return f'<Pet {self.name}, {self.species}>'
